f76303f572e666b733bb3a60dd8bb17ae0bfeac3
andrea <andrea@rpasolution.com>
2017-08-04 09:47:31 -0600

    Testing 4/8/2017

--- UnitTestProject1_Login/UnitTest1.cs
+++ UnitTestProject1_Login/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace UnitTestProject1_Login
     public class UnitTest1
     {
 
-        private string baseURL = "localhost:62008/Login.aspx";
+        private string baseURL = "http://localhost:62008/Login.aspx";
         private RemoteWebDriver driver = new ChromeDriver();
         public TestContext TestContext { get; set; }
 
@@ -25,11 +25,78 @@ namespace UnitTestProject1_Login
         {
             //Try to login with an invalid user
 
-            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(15));
-            driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(10));
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(20));
+            driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(15));
             driver.Navigate().GoToUrl(baseURL);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            //Find username form
+            IWebElement username = driver.FindElementById("TextBoxUsername");
+            username.Clear();
+            username.SendKeys("asdf@123.com");
+            wait.Until(ExpectedConditions.TextToBePresentInElementValue(By.Id("TextBoxUsername"), "asdf@123.com"));
+
+
+            //Find password form
+            IWebElement password = driver.FindElementById("TextBoxPassword");
+            password.Clear();
+            password.SendKeys("blahblah");
+            wait.Until(ExpectedConditions.TextToBePresentInElementValue(By.Id("TextBoxPassword"), "blahblah"));
+
+            IWebElement login_button = driver.FindElementById("ButtonLogin");
+            login_button.Click();
+
+            //wait.Until(ExpectedConditions.AlertIsPresent());
+            Assert.IsTrue(isAlertPresent(), "Test failed. It should display an alert");
+            driver.SwitchTo().Alert().Accept();
+
+
+            //Try to login with a valid user but without a password
+            username = driver.FindElementById("TextBoxUsername");
+            username.Clear();
+            username.SendKeys("andrea@rpasolution.com");
+
+            password = driver.FindElementById("TextBoxPassword");
+            password.Clear();
+
+            login_button = driver.FindElementById("ButtonLogin");
+            login_button.Click();
+
+            Assert.AreEqual(driver.Url, baseURL);
+
+
+            //Login using a valid username and password
+
+            username = driver.FindElementById("TextBoxUsername");
+            username.Clear();
+            username.SendKeys("SA");
+
+            password = driver.FindElementById("TextBoxPassword");
+            password.Clear();
+            password.SendKeys("airdogs");
+
+            login_button = driver.FindElementById("ButtonLogin");
+            login_button.Click();
+
+            driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(15));
+            Assert.AreEqual(driver.Url, "http://localhost:62008/Test.aspx");
+
 
         }
+
+
+        public bool isAlertPresent()
+        {
+            try
+            {
+                ExpectedConditions.AlertIsPresent();
+                return true;
+            }   // try 
+            catch (NoAlertPresentException Ex)
+            {
+                return false;
+            }   // catch 
+        }   // isAlertPresent()
 
         [TestCleanup()]
         public void MyTestCleanup()
@@ -45,7 +112,5 @@ namespace UnitTestProject1_Login
 
 
     }
-
-
 
 }
---
+++ UnitTestProject1_Login/UnitTest2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+
+namespace UnitTestProject1_Login
+{
+    [TestClass]
+    public class UnitTest2
+    {
+
+        private string baseURL = "http://localhost:62008/Login.aspx";
+        private RemoteWebDriver driver = new ChromeDriver();
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [TestCategory("Selenium")]
+        [Priority(1)]
+        [Owner("Chrome")]
+
+        public void TestMethod2()
+        {
+       
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(20));
+            driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(15));
+            driver.Navigate().GoToUrl(baseURL);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            //Login using a valid username and password
+
+            IWebElement username = driver.FindElementById("TextBoxUsername");
+            username.Clear();
+            username.SendKeys("SA");
+
+            IWebElement password = driver.FindElementById("TextBoxPassword");
+            password.Clear();
+            password.SendKeys("airdogs");
+
+            IWebElement login_button = driver.FindElementById("ButtonLogin");
+            login_button.Click();
+
+            driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(15));
+            Assert.AreEqual(driver.Url, "http://localhost:62008/Test.aspx");
+
+
+
+
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            driver.Quit();
+        }
+
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+
+        }
+
+
+    }
+}
